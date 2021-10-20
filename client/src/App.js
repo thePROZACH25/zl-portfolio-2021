10307cf04274248b0292d8275a6b32eb504001be
Zach S.L <zach.s.ledford@gmail.com>
2021-10-20 18:17:50 -0400

    adding hero buttons, icons and resume download button

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import Profile from './PortfolioContainer/Home/Profile'
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <Profile />
     </div>
   );
 }
