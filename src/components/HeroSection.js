6710e45c4e27f0b2cad781c883005009a9374098
Zach S.L <zach.s.ledford@gmail.com>
2021-10-22 14:23:40 -0400

    adding scroll arrows and social links

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import HeroImg from '../assets/images/DE640214-378F-4B20-BAE7-6BD2D436E181_1_105_c.jpeg'
+import SocialMediaArrow from '../assets/images/social-media-arrow.svg'
+import ScrollDownArrow from '../assets/images/scroll-down-arrow.svg'
 import PText from './PText'
 import Button from './Button'
 
@@ -23,6 +25,32 @@ export default function HeroSection() {
                     btnText='see my works'
                     outLine={false}
                     />
+                </div>
+                <div className="hero__social">
+                    <div className="hero__social__indicator">
+                        <p>Follow</p>
+                        <img src={SocialMediaArrow} alt="social media arrow" />
+                    </div>
+                    <div className="hero__social__text">
+                        <ul>
+                            <li>
+                                {/* GitHub Link */}
+                                <a href="github.com/thePROZACH25" target='_blank' rel='noreffer'>GH</a>
+                            </li>
+                            <li>
+                                {/* Twitter Link */}
+                                <a href="#" target='_blank' rel='noreffer'>TW</a>
+                            </li>
+                            <li>
+                                {/* LinkedIn Link */}
+                                <a href="linkedin.com/in/zacharyledford" target='_blank' rel='noreffer'>LI</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div className="hero__scrollDown">
+                    <p>Scroll</p>
+                    <img src={ScrollDownArrow} alt="" />
                 </div>
             </div>
         </div>
